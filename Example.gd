288e9523691466e154d9fa8b5072d82d5f3f7dca
Danny Garay <danilogaray60@gmail.com>
2022-09-11 13:08:48 -0600

    Update Example.gd

--- Example.gd
+++ Example.gd
@@ -10,4 +10,4 @@ func _on_StopAll_pressed() -> void:
 	$AudioStreamPlayerInteractive.stop_all(float($VBx/HBxFade/LineEdit.text))
 
 func _on_LinkButton_pressed() -> void:
-	pass # OS.shell_open("")
+	OS.shell_open("https://github.com/dannygaray60/interactive-music-g3#how-to-use")
